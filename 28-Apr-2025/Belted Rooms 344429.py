4fa3b88aad0de7037f2f45a5daa1a86194d3c77a
SabonaWaktole <133581145+SabonaWaktole@users.noreply.github.com>
2025-04-29 00:29:06 +0300

    Created 'Belted Rooms' by A2SV Hub

---
+++ 28-Apr-2025/Belted Rooms 344429.py
@@ -0,0 +1,32 @@
+# Problem: Belted Rooms - https://codeforces.com/problemset/problem/1428/B
+
+from collections import defaultdict
+
+test = int(input())
+for _ in range(test):
+    room = int(input())
+    edge = input()
+    
+    one_dir = True
+    for ch in edge:
+        if ch == '<':
+            one_dir = False
+            break
+    if one_dir:
+        print(room)
+        continue
+
+    other_dir = True
+    for ch in edge:
+        if ch == '>':
+            other_dir = False
+            break
+    if other_dir:
+        print(room)
+        continue
+
+    ans = 0
+    for i in range(room):
+        if edge[i] == '-' or edge[(i - 1) % room] == '-':
+            ans += 1
+    print(ans)
