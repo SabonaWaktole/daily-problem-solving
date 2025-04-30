ef8c39e0282c9a6e27e33eeda217da310ff07ef7
SabonaWaktole <133581145+SabonaWaktole@users.noreply.github.com>
2025-04-30 17:53:16 +0300

    Created 'Seat Reservation Manager' by A2SV Hub

---
+++ 30-Apr-2025/Seat Reservation Manager 345555.py
@@ -0,0 +1,20 @@
+# Problem: Seat Reservation Manager - https://leetcode.com/problems/seat-reservation-manager/description/
+
+class SeatManager:
+
+    def __init__(self, n: int):
+        seat = list(range(1, n+1))
+        self.unreserved = seat
+        heapify(self.unreserved)
+
+    def reserve(self) -> int:
+        return heappop(self.unreserved)
+
+    def unreserve(self, seatNumber: int) -> None:
+        heappush(self.unreserved, seatNumber)       
+
+
+# Your SeatManager object will be instantiated and called as such:
+# obj = SeatManager(n)
+# param_1 = obj.reserve()
+# obj.unreserve(seatNumber)
